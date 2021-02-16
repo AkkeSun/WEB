a209991d0c0381e5194e01e855d2352ac414240c
sun <78401555+AkkeSun@users.noreply.github.com>
2021-02-16 19:43:52 +0900

    Update Ex2.java

--- src/그리디/Ex2.java
+++ src/그리디/Ex2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Ex2 {
 	/*
+	
 	 [숫자카드게임]
 	 여러 개의 숫자 카드 중에서 가장 높은 숫자가 쓰인 카드 한 장을 뽑는 게임 
 	 1. int [][] = new int[M][N] 형태로 숫자가 주어진다
@@ -11,6 +12,10 @@ public class Ex2 {
 	 3. 선택한 [M] 중 가장 낮은 카드를 뽑아야한다. 
 	 4. 뽑은 카드들 중 가장 큰 카드를 가진 사람이 승리한다.
 	 5. 승리한 사람의 카드는 무엇인가 
+	 
+	 입력 : 3,3   1 2 5 2 1 2 4 5 3
+	 결과 : 3
+	
 	 */
 
 	public static void main(String[] args) {
@@ -25,7 +30,7 @@ public class Ex2 {
 		// 한 줄씩 입력 받아 확인하기
 		for (int i = 0; i < n; i++) { 
 			// 현재 줄에서 '가장 작은 수' 찾기
-			int min_value = 10001;
+			int min_value = 10001; // 초기화
 			for (int j = 0; j < m; j++) { 
 				int x = sc.nextInt(); 
 				min_value = Math.min(min_value, x);
