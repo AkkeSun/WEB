2f77b0a17f31c52cae80d5824533b4d2c3b26f94
sun <78401555+AkkeSun@users.noreply.github.com>
2021-02-17 20:11:55 +0900

    Update Ex2.java

--- src/구현/Ex2.java
+++ src/구현/Ex2.java
@@ -6,7 +6,7 @@ public class Ex2 {
 	  
 	 [시각]
 	 00시 00분 00초부터 N시 59분 59초까지 3이 들어가는 모든 경우의 수를 구하라
-	 
+	 입력 : 5  // 결과 : 11475
 	 */
 	
 	
@@ -20,8 +20,8 @@ public class Ex2 {
 	public static void main(String[] args) {
 
 		int cnt = 0;
-
-		for (int i = 0; i <= 5; i++) {
+		int n = 5;
+		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= 59; j++) {
 				for (int k = 0; k <= 59; k++) {
 					if(check(i,j,k)) cnt++;
