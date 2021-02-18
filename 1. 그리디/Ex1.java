133d828d63e8fb6ba99158bb65bfcab03ad01387
sun <78401555+AkkeSun@users.noreply.github.com>
2021-02-18 20:48:35 +0900

    Update Ex1.java

--- 1. 그리디/Ex1.java
+++ 1. 그리디/Ex1.java
@@ -14,31 +14,28 @@ public class Ex1 {
 	[큰 수의 법칙]
 	 다양한 수로 이루어진 배열이 있을 때 주어진 수들을 M번 더하여 가장 큰 수를 만드는 규칙 
 	 단, 배열의 특정한 인덱스에 해당하는 수가 연속해서 K번을 초과하여 더해질 수 없다. 
+	 주어진 배열 : {1,5,7,2,9}
+  	 입력 : 8 3   출력 : 68
 	 
 	 */
 	
 	public static void main(String[] args) {
-		Integer arr[] = {2,4,5,4,6};
-		int m = 8; int k =3;
+		Integer [] arr = {1,5,7,2,9};
+		int M = 8;
+		int K = 3; 
 		int result = 0;
 		Arrays.sort(arr, Collections.reverseOrder());
 		
 		while(true) {
-			for (int i = 0; i < k; i++) {
-				result += arr[0]; 
-				m--;
-				if(m == 0)
-					break;
+			for (int i = 0; i < K; i++) {
+				result += arr[0];
+				M --;
+				if(M ==0) break;
 			}
-			
 			result += arr[1];
-			m--;
-			if(m == 0)
-				break;
+			M--;
+			if (M==0) break;
 		}
-		
 		System.out.println(result);
-		
 	}
-
 }
